519de83e3d877dfd4585d7e101b67d6a6c7e907b
{cocoide} <cocoide@gmail.com>
2023-12-20 22:39:24 +0900

    delete: unused code

--- dependecy_injection/handler/handler.go
+++ dependecy_injection/handler/handler.go
@@ -4,10 +4,6 @@ import (
 	"github.com/cocoide/golang-design-pattern/dependecy_injection/usecase"
 )
 
-type BankHandlerParams struct {
-	Usecase *usecase.BankUsecase
-}
-
 func NewBankHandler(uc *usecase.BankUsecase) *BankHandler {
 	return &BankHandler{uc: uc}
 }
--- dependecy_injection/usecase/usecase.go
+++ dependecy_injection/usecase/usecase.go
@@ -4,10 +4,6 @@ import (
 	"github.com/cocoide/golang-design-pattern/dependecy_injection/repository"
 )
 
-type BankUsecaseParams struct {
-	Repository repository.Bank
-}
-
 func NewBankUsecase(repo repository.Bank) *BankUsecase {
 	return &BankUsecase{repo: repo}
 }
